89abd14acba110fe45e68d58c0b151f9e48a6ae6
saurabhjn76 <saurabhjn76@gmail.com>
2017-03-04 20:34:10 +0530

    chore: add package UI

--- app/src/main/java/saurabhjn76/com/capstoneproject/UI/MainActivity.java
+++ app/src/main/java/saurabhjn76/com/capstoneproject/UI/MainActivity.java
@@ -1,4 +1,4 @@
-package saurabhjn76.com.capstoneproject;
+package saurabhjn76.com.capstoneproject.UI;
 
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
@@ -8,6 +8,8 @@ import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+
+import saurabhjn76.com.capstoneproject.R;
 
 public class MainActivity extends AppCompatActivity {
 
