6957112e37e43f03c15b8edf944d80b08b1e854b
saurabhjn76 <saurabhjn76@gmail.com>
2017-03-17 03:41:36 +0530

    implemented nav drawer in main activity

--- app/src/main/java/saurabhjn76/com/capstoneproject/ui/MainActivity.java
+++ app/src/main/java/saurabhjn76/com/capstoneproject/ui/MainActivity.java
@@ -7,11 +7,15 @@ import android.net.ConnectivityManager;
 import android.net.NetworkInfo;
 import android.os.Parcelable;
 import android.os.PersistableBundle;
+import android.support.design.widget.NavigationView;
 import android.support.v4.app.FragmentManager;
 import android.content.Context;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
+import android.support.v4.view.GravityCompat;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
@@ -29,7 +33,7 @@ import java.util.ArrayList;
 import saurabhjn76.com.capstoneproject.Adapter.ImageAdapter;
 import saurabhjn76.com.capstoneproject.R;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity  implements NavigationView.OnNavigationItemSelectedListener{
     public  View v;
     public View s;
     boolean mTwoPane=true;
@@ -75,6 +79,14 @@ public class MainActivity extends AppCompatActivity {
 
 
         setSupportActionBar(toolbar);
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
+                this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
+        drawer.setDrawerListener(toggle);
+        toggle.syncState();
+
+        NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
+        navigationView.setNavigationItemSelectedListener(this);
 
     }
 
@@ -152,6 +164,36 @@ public class MainActivity extends AppCompatActivity {
         }
 
     }
+    @SuppressWarnings("StatementWithEmptyBody")
+    @Override
+    public boolean onNavigationItemSelected(MenuItem item) {
+        // Handle navigation view item clicks here.
+        int id = item.getItemId();
+
+        if (id == R.id.profile) {
+            // Handle the camera action
+            //Intent intent = new Intent(PersonalBoard.this, ProfileActivity.class);
+            //startActivity(intent);
+        } else if (id == R.id.changeEmail) {
+            //Intent intent = new Intent(PersonalBoard.this, CreateBoard.class);
+           // startActivity(intent);
+        }else if (id == R.id.delete_profile) {
+        //    Intent intent = new Intent(PersonalBoard.this, MainActivity.class);
+          //  startActivity(intent);
+        }
+        else if (id == R.id.nav_update_profile) {
+
+        } else if (id == R.id.nav_logout) {
+         //   firebaseAuth.signOut();
+           // Intent intent = new Intent(PersonalBoard.this, WelcomeActivity.class);
+            //startActivity(intent);
+        }
+
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        drawer.closeDrawer(GravityCompat.START);
+        return true;
+    }
+
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
@@ -165,30 +207,17 @@ public class MainActivity extends AppCompatActivity {
         // Handle action bar item clicks here. The action bar will
         // automatically handle clicks on the Home/Up button, so long
         // as you specify a parent activity in AndroidManifest.xml.
-       /* int id = item.getItemId();
-        MainFragment fragment = MainFragment.instance;
+        int id = item.getItemId();
 
         //noinspection SimplifiableIfStatement
-        if (id == R.id.action_popularity) {
-            fragment.sort_order = "popular";
-            fragment.update(fragment.sort_order);
-
-
-
+        if (id == R.id.action_settings) {
+            return true;
         }
-        if(id==R.id.action_rated)
-        {
-            fragment.sort_order = "top_rated";
-            fragment.update(fragment.sort_order);
-        }
-        if(id==R.id.action_fav)
-        {
-            fragment.sort_order="favourites";
-            fragment.update(fragment.sort_order);
-        }*/
 
         return super.onOptionsItemSelected(item);
     }
+
+
     public boolean checknetconnection()
     {
         ConnectivityManager manager_c =(ConnectivityManager) getApplicationContext().getSystemService(CONNECTIVITY_SERVICE);
