6b574e9f1ea28164c73b4ed13f145bb2e053a455
saurabhjn76 <saurabhjn76@gmail.com>
2017-03-17 00:50:14 +0530

    add login activity

---
+++ app/src/main/java/saurabhjn76/com/capstoneproject/ui/LoginActivity.java
@@ -0,0 +1,15 @@
+package saurabhjn76.com.capstoneproject.ui;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import saurabhjn76.com.capstoneproject.R;
+
+public class LoginActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+    }
+}
