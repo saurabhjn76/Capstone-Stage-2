3c2bd368c91dfba9eede171780417fdf890ed232
saurabhjn76 <saurabhjn76@gmail.com>
2017-03-19 04:21:29 +0530

    add leader board activity

---
+++ app/src/main/java/saurabhjn76/com/capstoneproject/ui/LeaderBoardActivity.java
@@ -0,0 +1,15 @@
+package saurabhjn76.com.capstoneproject.ui;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import saurabhjn76.com.capstoneproject.R;
+
+public class LeaderBoardActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_leader_board);
+    }
+}
