dc748c6f160a11a7e0363b3827566d72985d5eed
saurabhjn76 <saurabhjn76@gmail.com>
2017-03-19 15:58:03 +0530

    added toolbar heading

--- app/src/main/java/saurabhjn76/com/capstoneproject/ui/DetailActivity.java
+++ app/src/main/java/saurabhjn76/com/capstoneproject/ui/DetailActivity.java
@@ -4,10 +4,12 @@ import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
 import android.widget.LinearLayout;
 
 import com.android.volley.RequestQueue;
 
+import saurabhjn76.com.capstoneproject.Adapter.ImageAdapter;
 import saurabhjn76.com.capstoneproject.Fragment.DetailFragment;
 import saurabhjn76.com.capstoneproject.R;
 
@@ -17,6 +19,9 @@ public class DetailActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
 
         setContentView(R.layout.activity_detail);
+        Toolbar toolbar = (Toolbar)findViewById(R.id.toolbar);
+        toolbar.setTitle(ImageAdapter.mCatIds[getIntent().getIntExtra("POSITION",-23)]);
+
 
         FragmentManager manager = getSupportFragmentManager();
         Fragment frag =new DetailFragment();
