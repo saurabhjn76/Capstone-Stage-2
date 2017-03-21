10dfd225a9272cd32b82d27390d0e08bee0894ef
saurabhjn76 <saurabhjn76@gmail.com>
2017-03-22 02:13:59 +0530

    added new files for collection widget

---
+++ app/src/main/java/saurabhjn76/com/capstoneproject/Widget/WidgetDataProvider.java
@@ -0,0 +1,83 @@
+package saurabhjn76.com.capstoneproject.Widget;
+
+/**
+ * Created by saurabh on 22/3/17.
+ */
+import java.util.ArrayList;
+import java.util.List;
+
+import android.annotation.SuppressLint;
+import android.content.Context;
+import android.content.Intent;
+import android.graphics.Color;
+import android.widget.RemoteViews;
+import android.widget.RemoteViewsService.RemoteViewsFactory;
+
+@SuppressLint("NewApi")
+class WidgetDataProvider implements RemoteViewsFactory {
+
+    List mCollections = new ArrayList();
+
+    Context mContext = null;
+
+    public WidgetDataProvider(Context context, Intent intent) {
+        mContext = context;
+    }
+
+    @Override
+    public int getCount() {
+        return mCollections.size();
+    }
+
+    @Override
+    public long getItemId(int position) {
+        return position;
+    }
+
+    @Override
+    public RemoteViews getLoadingView() {
+        return null;
+    }
+
+    @Override
+    public RemoteViews getViewAt(int position) {
+        RemoteViews mView = new RemoteViews(mContext.getPackageName(),
+                android.R.layout.simple_list_item_1);
+        mView.setTextViewText(android.R.id.text1, (CharSequence) mCollections.get(position));
+        mView.setTextColor(android.R.id.text1, Color.BLACK);
+        return mView;
+    }
+
+    @Override
+    public int getViewTypeCount() {
+        return 1;
+    }
+
+    @Override
+    public boolean hasStableIds() {
+        return true;
+    }
+
+    @Override
+    public void onCreate() {
+        initData();
+    }
+
+    @Override
+    public void onDataSetChanged() {
+        initData();
+    }
+
+    private void initData() {
+        mCollections.clear();
+        for (int i = 1; i <= 10; i++) {
+            mCollections.add("ListView item " + i);
+        }
+    }
+
+    @Override
+    public void onDestroy() {
+
+    }
+
+}
