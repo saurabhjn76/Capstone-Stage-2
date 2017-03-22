f0acaaad2c6a900ea3342772737478e66ef956b5
saurabhjn76 <saurabhjn76@gmail.com>
2017-03-22 15:46:53 +0530

    modified leader board activity

--- app/src/main/java/saurabhjn76/com/capstoneproject/ui/LeaderBoardActivity.java
+++ app/src/main/java/saurabhjn76/com/capstoneproject/ui/LeaderBoardActivity.java
@@ -1,8 +1,11 @@
 package saurabhjn76.com.capstoneproject.ui;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
 import saurabhjn76.com.capstoneproject.R;
 
 public class LeaderBoardActivity extends AppCompatActivity {
@@ -11,5 +14,18 @@ public class LeaderBoardActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_leader_board);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+       FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
+            }
+        });
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
     }
+
 }
