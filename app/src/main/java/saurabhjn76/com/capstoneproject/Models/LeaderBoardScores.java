beafb4dd4de05f72e77bf15dcab3d444beaa1bd1
saurabhjn76 <saurabhjn76@gmail.com>
2017-03-22 15:47:16 +0530

    modified leader board,added leaderboard model

--- app/src/main/java/saurabhjn76/com/capstoneproject/Models/LeaderBoardScores.java
+++ app/src/main/java/saurabhjn76/com/capstoneproject/Models/LeaderBoardScores.java
@@ -1,7 +1,88 @@
 package saurabhjn76.com.capstoneproject.Models;
 
+import android.os.Parcel;
+import android.os.Parcelable;
+
 /**
  * Created by saurabh on 22/3/17.
  */
-public class LeaderBoardScores {
+public class LeaderBoardScores  implements Parcelable {
+    public String userid;
+    public String user_name;
+    public String name;
+    public int scores;
+    public String date;
+    public String level;
+
+    public LeaderBoardScores(){}
+
+    public LeaderBoardScores(String userid, String user_name,String name, int scores, String date, String level)
+    {
+        this.userid=userid;
+        this.user_name=user_name;
+        this.name=name;
+        this.level=level;
+        this.date=date;
+        this.scores=scores;
+    }
+    protected LeaderBoardScores(Parcel in) {
+        name = in.readString();
+        scores = in.readInt();
+        date=in.readString();
+        level = in.readString();
+        userid=in.readString();
+        user_name=in.readString();
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getUser_name() {
+        return user_name;
+    }
+
+    public String getUserid() {
+        return userid;
+    }
+
+    public int getScores() {
+        return scores;
+    }
+
+    public String getDate() {
+        return date;
+    }
+
+    public String getLevel() {
+        return level;
+    }
+
+    @Override
+    public int describeContents() {
+        return 0;
+    }
+
+    @Override
+    public void writeToParcel(Parcel parcel, int flags) {
+        parcel.writeString(name);
+        parcel.writeString(level);
+        parcel.writeInt(scores);
+        parcel.writeString(userid);
+        parcel.writeString(user_name);
+        parcel.writeString(date);
+
+    }
+
+    public static final Parcelable.Creator<LeaderBoardScores> CREATOR = new Parcelable.Creator<LeaderBoardScores>() {
+        @Override
+        public LeaderBoardScores createFromParcel(Parcel parcel) {
+            return new LeaderBoardScores(parcel);
+        }
+
+        @Override
+        public LeaderBoardScores[] newArray(int i) {
+            return new LeaderBoardScores[i];
+        }
+    };
 }
--- app/src/main/java/saurabhjn76/com/capstoneproject/ui/LeaderBoardActivity.java
+++ app/src/main/java/saurabhjn76/com/capstoneproject/ui/LeaderBoardActivity.java
@@ -5,27 +5,69 @@ import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.View;
+
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+import saurabhjn76.com.capstoneproject.Models.User;
 import saurabhjn76.com.capstoneproject.R;
 
 public class LeaderBoardActivity extends AppCompatActivity {
 
+    private FirebaseAuth auth;
+    private DatabaseReference mFirebaseDatabase;
+    private FirebaseDatabase mFirebaseInstance;
+    private String userId;
+    private static final String TAG = LeaderBoardActivity.class.getSimpleName();
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_leader_board);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        auth = FirebaseAuth.getInstance();
+        mFirebaseInstance = FirebaseDatabase.getInstance();
+        // get reference to 'users' node
+        mFirebaseDatabase = mFirebaseInstance.getReference("scores");
+        userId =auth.getCurrentUser().getUid();
+      //  User user = new User(name, email);
+        mFirebaseDatabase.child(userId).setValue("");
+        addScoreChangeListener();
 
-       FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
+
+    }
+    /**
+     * User data change listener
+     */
+    private void addScoreChangeListener() {
+        // User data change listener
+        mFirebaseDatabase.child(userId).addValueEventListener(new ValueEventListener() {
             @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                User user = dataSnapshot.getValue(User.class);
+
+                // Check for null
+                if (user == null) {
+                    Log.e(TAG, "User data is null!");
+                    return;
+                }
+
+                Log.e(TAG, "User data is changed!" + user.name + ", " + user.email);
+            }
+
+            @Override
+            public void onCancelled(DatabaseError error) {
+                // Failed to read value
+                Log.e(TAG, "Failed to read user", error.toException());
             }
         });
-        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
     }
 
 }
