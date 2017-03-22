7a5b20d3c805c9c18d4fb42b8b3e0e4b568ffa38
saurabhjn76 <saurabhjn76@gmail.com>
2017-03-22 21:46:22 +0530

    removed unused menu

--- app/src/main/java/saurabhjn76/com/capstoneproject/ui/MainActivity.java
+++ app/src/main/java/saurabhjn76/com/capstoneproject/ui/MainActivity.java
@@ -221,14 +221,6 @@ public class MainActivity extends AppCompatActivity  implements NavigationView.O
         return true;
     }
 
-
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
-        return true;
-    }
-
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
         // Handle action bar item clicks here. The action bar will
